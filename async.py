da71639c0d600010fc14ae2d23224f6f363ce289
Clemens Sielaff <git@clemens-sielaff.com>
2022-07-19 11:37:20 +0200

    2.2 - generator handling

--- async.py
+++ async.py
@@ -3,40 +3,45 @@ from typing import *
 
 
 class Scheduler:
-    ready: List[Callable] = list()
-    sleeping: List[Tuple[float, Callable]] = list()  # sleeping functions
+    ready: List[Generator] = list()
+    sleeping: List[Tuple[float, Generator]] = list()
 
     @classmethod
-    def call_soon(cls, func: Callable):
+    def call_soon(cls, func: Generator):
         cls.ready.append(func)
 
     @classmethod
-    def call_later(cls, delay: float, func: Callable):
+    def call_later(cls, delay: float, func: Generator):
         deadline = now() + delay
         cls.sleeping.append((deadline, func))
-        cls.sleeping.sort()  # sort by expiration
+        cls.sleeping.sort()
 
     @classmethod
     def run(cls):
         while cls.ready or cls.sleeping:
             if not cls.ready:
-                deadline, func = cls.sleeping.pop(0)  # get next
+                deadline, func = cls.sleeping.pop(0)
                 delta = deadline - now()
                 if delta > 0:
-                    sleep(delta)  # sleep until next wakes up
+                    sleep(delta)
                 cls.call_soon(func)
             while cls.ready:
                 current = cls.ready.pop(0)
-                current()
+                try:
+                    delay = next(current)
+                except StopIteration:
+                    continue  # discard finished generators
+                if delay is not None:
+                    cls.call_later(delay, current)
 
 
 def countdown(name: str, n: int):
-    if n >= 0:
+    while n >= 0:
         print(name, n)
-        # no sleep
-        Scheduler.call_later(1, lambda: countdown(name, n - 1))
+        yield 1  # sleep for 1 second
+        n -= 1
 
 
-Scheduler.call_soon(lambda: countdown("Alice ", 3))
-Scheduler.call_soon(lambda: countdown("Bob ", 3))
+Scheduler.call_soon(countdown("Alice ", 3))
+Scheduler.call_soon(countdown("Bob ", 3))
 Scheduler.run()

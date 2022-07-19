b5555390f2e6260a2cfc559192e2edfbecc7571e
Clemens Sielaff <git@clemens-sielaff.com>
2022-07-19 11:44:47 +0200

    3.5 - consumer / producer

--- async.py
+++ async.py
@@ -2,16 +2,38 @@ from time import sleep, time as now
 from typing import *
 
 
+class Task:
+    def __init__(self, generator: Generator):
+        self.generator: Generator = generator
+        self.result: Optional[Any] = None
+
+
+class Sleep:
+    def __init__(self, duration: float):
+        self.duration: float = duration
+
+
+class Result:
+    def __init__(self, result: Any):
+        self.result: Any = result
+
+
+class Await:
+    def __init__(self, task: Task):
+        self.task: Task = task
+
+
 class Scheduler:
-    ready: List[Generator] = list()
-    sleeping: List[Tuple[float, Generator]] = list()
+    ready: List[Task] = list()
+    sleeping: List[Tuple[float, Task]] = list()
+    blocked: Dict[Task, Set[Task]] = dict()
 
     @classmethod
-    def call_soon(cls, func: Generator):
+    def call_soon(cls, func: Task):
         cls.ready.append(func)
 
     @classmethod
-    def call_later(cls, delay: float, func: Generator):
+    def call_later(cls, delay: float, func: Task):
         deadline = now() + delay
         cls.sleeping.append((deadline, func))
         cls.sleeping.sort()
@@ -28,20 +50,42 @@ class Scheduler:
             while cls.ready:
                 current = cls.ready.pop(0)
                 try:
-                    delay = next(current)
+                    instruction = next(current.generator)
                 except StopIteration:
-                    continue  # discard finished generators
-                if delay is not None:
-                    cls.call_later(delay, current)
+                    continue
+
+                if isinstance(instruction, Sleep):
+                    cls.call_later(instruction.duration, current)
+
+                elif isinstance(instruction, Result):
+                    current.result = instruction.result
+                    # reschedule blocked tasks
+                    if current in cls.blocked:
+                        for blocked in cls.blocked[current]:
+                            cls.call_soon(blocked)
+                        del cls.blocked[current]
+
+                elif isinstance(instruction, Await):
+                    if instruction.task in cls.blocked:
+                        cls.blocked[instruction.task].add(current)
+                    else:
+                        cls.blocked[instruction.task] = {current}
+                    cls.call_soon(instruction.task)
 
 
-def countdown(name: str, n: int):
-    while n >= 0:
-        print(name, n)
-        yield 1  # sleep for 1 second
-        n -= 1
+def producer(n: int):
+    yield Sleep(1)
+    yield Result(n - 1)
 
 
-Scheduler.call_soon(countdown("Alice ", 3))
-Scheduler.call_soon(countdown("Bob ", 3))
+def countdown(name: str, x: int):
+    while x >= 0:
+        print(name, x)
+        task = Task(producer(x))
+        yield Await(task)
+        x = task.result
+
+
+Scheduler.call_soon(Task(countdown("Alice ", 3)))
+Scheduler.call_soon(Task(countdown("Bob ", 3)))
 Scheduler.run()
